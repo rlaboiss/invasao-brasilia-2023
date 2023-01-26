c20d3f95ea37a0105cc0c2915f2f5fe1244b31d7
Rafael Laboissière <rafael@laboissiere.net>
2023-01-26 22:21:01 +0100

    Proper printing of results

--- analise.r
+++ analise.r
@@ -1,20 +1,27 @@
 presos <- read.csv ("presos.csv")
-table (presos$genero)
+
+cat ("Número de presos por gênero:\n")
+print (table (presos$genero))
 
 presos$data.nascimento <- as.Date (presos$data.nascimento)
 presos$idade <- as.numeric (as.Date ("2023-01-08")
                             - presos$data.nascimento) / 365.25
-summary (presos$idade)
+
+cat ("\nEstatíticas descriptivas por idade:\n")
+print (summary (presos$idade))
 
 n <- nrow (presos)
-length (which (presos$idade > 45 & presos$idade < 55)) / n
-length (which (presos$idade > 40 & presos$idade < 60)) / n
+print (length (which (presos$idade > 45 & presos$idade < 55)) / n)
+print (length (which (presos$idade > 40 & presos$idade < 60)) / n)
 
-(mean.df <- aggregate (presos, idade ~ genero, mean))
+mean.df <- aggregate (presos, idade ~ genero, mean)
+cat ("\nIdades médias por gênero:\n")
+print (mean.df)
 mean.f <- mean.df$idade [1]
 mean.m <- mean.df$idade [2]
-aggregate (presos, idade ~ genero, median)
-t.test (idade ~ genero, presos)
+cat ("\nIdades medianas por gênero:\n")
+print (aggregate (presos, idade ~ genero, median))
+print (t.test (idade ~ genero, presos))
 
 h <- hist (presos$idade, plot = FALSE)
 max.count <- max (h$counts)
@@ -37,6 +44,12 @@ legend ("topleft", legend = c("mulheres", "homens"), pch = 15,
         col = c("pink", "cyan"), inset = 0.05)
 dev.off ()
 
+cat ("\nNúmero de presos sem UF indicada:\n")
+print (length (which (presos$uf == "")))
+
+uf.count <- sort (table (presos$uf), decreasing = TRUE)
+uf.count <- uf.count [which (names (uf.count) != "")]
+
 election <- read.csv ("eleicao-2022.csv")
 population <- read.csv ("populacao-2022.csv")
 election$count <- election$count.pop <- NA
@@ -47,8 +60,6 @@ for (i in names(uf.count)) {
         / population$populacao [which (population$uf == i)])
 }
 
-uf.count <- sort (table (presos$uf), decreasing = TRUE)
-uf.count <- uf.count [which (names (uf.count) != "")]
 cat ("\n|estado|número de pessoas presas|porcentagem do total|presos/população|\n|-|-|-|-|\n")
 s <- sum (uf.count)
 for (i in names (uf.count))
@@ -57,7 +68,9 @@ for (i in names (uf.count))
                   election$count.pop [which (election$uf == i)]))
 
 election <- subset (election, count > 5)
-cor.test (election$votos.bolsonaro, log (election$count.pop))
+
+cat ("\nTest de correlação entre votos en Bolsonaro e número de presos por estado:\n")
+print (cor.test (election$votos.bolsonaro, log (election$count.pop)))
 
 png (file = "votos-bolsonaro-numero-presos.png", height = 640, width = 640,
      pointsize = 24)
